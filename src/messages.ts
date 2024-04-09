f3802f744368ffda2440b20b731f02e2fd120660
Andrew A. Barber <hello@andrewbarber.me>
2024-04-09 12:47:25 +0100

    Added context to message logs

--- src/messages.ts
+++ src/messages.ts
@@ -1,22 +1,23 @@
-import chalk from 'chalk';
+import chalk from "chalk";
 
 function f(message: string) {
-    return `[${new Date().toISOString()}] ${message}`;
+  return `[${new Date().toISOString()}] ${message}`;
 }
 
 /* eslint-disable no-console */
 export const log = {
-    debug: (message: string) => console.debug(chalk.grey(f(message))),
-    info: (message: string) => console.log(chalk.blue(f(message))),
-    notice: (message: string) => console.log(chalk.yellow(f(message))),
-    error: (message: string) => console.error(chalk.red(f(message))),
-    success: (message: string) => console.log(chalk.green(f(message))),
+  debug: (message: string) => console.debug(chalk.grey(f(message))),
+  info: (message: string) => console.log(chalk.blue(f(message))),
+  notice: (message: string) => console.log(chalk.yellow(f(message))),
+  error: (message: string) => console.error(chalk.red(f(message))),
+  success: (message: string) => console.log(chalk.green(f(message))),
 };
 /* eslint-enable no-console */
 export const messages = {
-    invalidAuth: (configKey: string) => `${configKey} is invalid.`,
-    invalidEmailTemplateType: 'Invalid email template type.',
-    emailTemplateExists: 'Email template already exists. Please supply with overtwrite flag if you want to overwrite it.',
-    noEmailTemplateFound: 'No email template found.',
+  invalidAuth: (configKey: string) => `${configKey} is invalid.`,
+  invalidEmailTemplateType: (templateName: string) => `Invalid email template type - ${templateName}.`,
+  emailTemplateExists: (templateName: string) =>
+    `Email template (${templateName}) already exists. Please supply with overwrite flag if you want to overwrite it.`,
+  noEmailTemplateFound: (templateName: string) => `No email template found - ${templateName}.`,
 };
 /* eslint-enable max-len */
--- src/services/emailTemplate/downloadEmailTemplate.ts
+++ src/services/emailTemplate/downloadEmailTemplate.ts
@@ -1,4 +1,3 @@
-
 import fs from "fs";
 import { Management } from "@space48/bigcommerce-api";
 import { omit } from "lodash";
@@ -6,67 +5,59 @@ import dotenv from "dotenv";
 import { messages, log } from "../../messages";
 dotenv.config();
 
-
 const bcClient = new Management.Client({
-    accessToken: process.env.BC_ACCESS_TOKEN as string,
-    storeHash: process.env.BC_STORE_HASH as string,
+  accessToken: process.env.BC_ACCESS_TOKEN as string,
+  storeHash: process.env.BC_STORE_HASH as string,
 });
 
 const downloadEmailTemplate = async (templateName: string, path: string, overwrite: false) => {
-    try{
-        const isFetchAll = templateName === 'all';
+  try {
+    const isFetchAll = templateName === "all";
 
-        if(isFetchAll){
-            const emailTemplatesResponse = await getAllEmailTemplates(bcClient);
-            if(!emailTemplatesResponse) return log.error(messages.noEmailTemplateFound);
-
-        }
-
-        if(!isFetchAll){
-            const emailTemplateResponse = await getEmailTemplate(bcClient, templateName);
-            if(!emailTemplateResponse) return log.error(messages.noEmailTemplateFound);
-
-            // create a directory for the email template
-            // structure will be like this: path/templateName/
-
-            const emailTemplateDir = `${path}/${templateName}`;
-            if(fs.existsSync(emailTemplateDir) && !overwrite){
-                return log.error(messages.emailTemplateExists);
-            } 
-            fs.mkdirSync(emailTemplateDir, {recursive: true});
-
-            // write the email template to a file
-            await Promise.all([
-                fs.writeFileSync(`${emailTemplateDir}/${templateName}.json`, JSON.stringify(omit(emailTemplateResponse, "body")) ),
-                fs.writeFileSync(`${emailTemplateDir}/${templateName}.hbs`, emailTemplateResponse.body),
-            ]);
-        }
-
-    } catch (error) {
-        log.error(error);
-
+    if (isFetchAll) {
+      const emailTemplatesResponse = await getAllEmailTemplates(bcClient);
+      if (!emailTemplatesResponse) return log.error(messages.noEmailTemplateFound(templateName));
     }
 
+    if (!isFetchAll) {
+      const emailTemplateResponse = await getEmailTemplate(bcClient, templateName);
+      if (!emailTemplateResponse) return log.error(messages.noEmailTemplateFound(templateName));
+
+      // create a directory for the email template
+      // structure will be like this: path/templateName/
+
+      const emailTemplateDir = `${path}/${templateName}`;
+      if (fs.existsSync(emailTemplateDir) && !overwrite) {
+        return log.error(messages.emailTemplateExists);
+      }
+      fs.mkdirSync(emailTemplateDir, { recursive: true });
+
+      // write the email template to a file
+      await Promise.all([
+        fs.writeFileSync(`${emailTemplateDir}/${templateName}.json`, JSON.stringify(omit(emailTemplateResponse, "body"))),
+        fs.writeFileSync(`${emailTemplateDir}/${templateName}.hbs`, emailTemplateResponse.body),
+      ]);
+    }
+  } catch (error) {
+    log.error(error);
+  }
 };
 
-
 const getAllEmailTemplates = async (
-    bigCommerceApiClient: Management.Client,
-    query?: Management.V3.Operations["GET /marketing/email-templates"]["parameters"]["query"],  
-) => await bigCommerceApiClient.v3.get('/marketing/email-templates', {query});
-    
+  bigCommerceApiClient: Management.Client,
+  query?: Management.V3.Operations["GET /marketing/email-templates"]["parameters"]["query"],
+) => await bigCommerceApiClient.v3.get("/marketing/email-templates", { query });
 
 const getEmailTemplate = async (
-    bigCommerceApiClient: Management.Client,
-    templateName: string,
-    query?: Management.V3.Operations["GET /marketing/email-templates/{template-name}"]["parameters"]["query"],
-) => await bigCommerceApiClient.v3.get(`/marketing/email-templates/{template-name}`, {
-        path: {
-            "template-name": templateName,
-        },
-        query,
-    });
-
-
+  bigCommerceApiClient: Management.Client,
+  templateName: string,
+  query?: Management.V3.Operations["GET /marketing/email-templates/{template-name}"]["parameters"]["query"],
+) =>
+  await bigCommerceApiClient.v3.get(`/marketing/email-templates/{template-name}`, {
+    path: {
+      "template-name": templateName,
+    },
+    query,
+  });
 
 export default downloadEmailTemplate;
